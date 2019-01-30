3362b625cc121f0ab58cebf2e368000d826a99b8
Alexx <d5enemy@gmail.com>
2019-01-30 16:31:40 +0200

    Applied all new changes

---
+++ Resourses/Scripts/Gun.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public enum FireMode { Auto, Burst, Single};
+    public FireMode fireMode;
+
+    public Transform[] projectileSpawn;
+    public Projectile projectile;
+    public float msBetweenShots = 100;
+    public float muzzleVelocity = 35;
+    public int burstCount;
+
+    public Transform shell;
+    public Transform shellEjection;
+    MuzzleFlash muzzleflash;
+    float nextShotTime;
+
+    bool triggerReleaseSinceLastShot;
+    int shotsRemainingInBurst;
+
+    private void Start()
+    {
+        muzzleflash = GetComponent<MuzzleFlash>();
+        shotsRemainingInBurst = burstCount;
+    }    
+
+    void Shoot()
+    {
+        if(Time.time > nextShotTime)
+        {
+            if (fireMode == FireMode.Burst)
+            {
+                if (shotsRemainingInBurst == 0)
+                {
+                    return;
+                }
+                shotsRemainingInBurst--;
+            }
+            else if (fireMode == FireMode.Single)
+            {
+                if (!triggerReleaseSinceLastShot)
+                {
+                    return;
+                }
+            }
+
+            for (int i = 0; i < projectileSpawn.Length; i++)
+            {
+                nextShotTime = Time.time + msBetweenShots / 1000;
+                Projectile newProjectile = Instantiate(projectile, projectileSpawn[i].position, projectileSpawn[i].rotation) as Projectile;
+                newProjectile.SetSpeed(muzzleVelocity);
+            }
+
+            Instantiate(shell, shellEjection.position, shellEjection.rotation);
+            muzzleflash.Activate();
+        }
+    }
+
+    public void OnTriggerHold()
+    {
+        Shoot();
+        triggerReleaseSinceLastShot = false;
+    }
+
+    public void OnTriggerRelease()
+    {
+        triggerReleaseSinceLastShot = true;
+        shotsRemainingInBurst = burstCount;
+    }
+
+}
